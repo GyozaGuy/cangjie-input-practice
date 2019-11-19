ba8fe1c6bcef8f0f3bd2d53d121a99f01d8a870e
Steven Stromberg <steven.stromberg@familysearch.org>
2019-11-19 14:57:27 -0700

    Added the ability to choose which HSK lists to practice

--- public/components/cangjieInput/cangjieInput.mjs
+++ public/components/cangjieInput/cangjieInput.mjs
@@ -25,7 +25,7 @@ customElements.define(
           cangjie-input .cangjieInput_testCharacter[incorrect] {
             color: Red;
           }
-          cangjie-input input {
+          cangjie-input input[type="text"] {
             border: 0;
             border-radius: 4px;
             box-shadow: inset 0 2px 4px Grey;
@@ -33,6 +33,9 @@ customElements.define(
             outline: 0;
             padding: 10px;
             width: 100%;
+          }
+          cangjie-input .cangjieInput_charListContainer {
+            margin-top: 20px;
           }
           cangjie-input .cangjieInput_descContainer {
             margin-top: 20px;
@@ -53,6 +56,8 @@ customElements.define(
         <div class="cangjieInput_testCharacter"></div>
 
         <input type="text">
+
+        <div class="cangjieInput_charListContainer"></div>
 
         <div class="cangjieInput_descContainer">
           <div>
@@ -76,14 +81,29 @@ customElements.define(
       `
 
       this.testCharacterBox = this.querySelector('.cangjieInput_testCharacter')
+      this.charListContainer = this.querySelector('.cangjieInput_charListContainer')
+      this.descContainer = this.querySelector('.cangjieInput_descContainer')
       this.cangjieCodeBox = this.querySelector('.cangjieInput_cangjieCode')
       this.alphaCodeBox = this.querySelector('.cangjieInput_alphaCode')
-      this.descContainer = this.querySelector('.cangjieInput_descContainer')
       this.simpCharBox = this.querySelector('.cangjieInput_simpChar')
       this.pronunciationBox = this.querySelector('.cangjieInput_pronunciation')
       this.definitionBox = this.querySelector('.cangjieInput_definition')
 
       const input = this.querySelector('input')
+
+      this.addEventListener('click', ({ target }) => {
+        if (target.matches('input[type="checkbox"]')) {
+          localStorage.setItem(
+            'cangjieInput_selectedCharacterLists',
+            JSON.stringify(this.characterLists)
+          )
+        }
+      })
+
+      input.addEventListener('input', async ({ target: { value } }) => {
+        const { cangjie } = await getCharactersForCode(value)
+        input.value = cangjie
+      })
 
       input.addEventListener('keypress', async event => {
         const {
@@ -112,18 +132,29 @@ customElements.define(
         }
       })
 
-      input.addEventListener('input', async ({ target: { value } }) => {
-        const { cangjie } = await getCharactersForCode(value)
-        input.value = cangjie
+      const allCharacterLists = await getCharacterLists()
+
+      allCharacterLists.forEach(list => {
+        this.charListContainer.innerHTML += `
+          <label>
+            <input type="checkbox" value="${list}"> ${list}
+          </label>
+        `
       })
+
+      const cachedCharacterLists = localStorage.getItem('cangjieInput_selectedCharacterLists')
+
+      if (cachedCharacterLists) {
+        this.characterLists = JSON.parse(cachedCharacterLists)
+      } else {
+        this.characterLists = allCharacterLists
+      }
 
       this.selectRandomTestCharacter()
     }
 
     async selectRandomTestCharacter() {
-      // TODO: select character lists
-      const characterLists = await getCharacterLists()
-      const { characters, codes, english, pinyin } = await getRandomCharacter(characterLists)
+      const { characters, codes, english, pinyin } = await getRandomCharacter(this.characterLists)
       const { simp, trad } = characters
 
       this.character = trad
@@ -139,6 +170,18 @@ customElements.define(
 
     set character(character) {
       this.testCharacterBox.textContent = character
+    }
+
+    get characterLists() {
+      const checkboxes = this.querySelectorAll('input[type="checkbox"]')
+      return [...checkboxes].filter(cb => cb.checked).map(cb => cb.value)
+    }
+
+    set characterLists(characterLists) {
+      const checkboxes = this.querySelectorAll('input[type="checkbox"]')
+      checkboxes.forEach(cb => {
+        cb.checked = characterLists.includes(cb.value)
+      })
     }
 
     set codes({ alpha, cangjie }) {
